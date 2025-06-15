2df0efaeba2965825e26ff067d11d06b71389771
António Kalumana <ak@kalumana-mbp.lan>
2025-06-15 01:50:15 +0100

    Uncomment on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-//import "./index.css"; // or keep your CSS/SCSS structure
+import "./index.css"; // or keep your CSS/SCSS structure
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
