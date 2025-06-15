19bd6bf7768b35e5795249b2dd86389e9249bc67
António Kalumana <ak@kalumana-mbp.lan>
2025-06-15 01:28:44 +0100

    Updating vite file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortifolio'
+  //server: { port: 3000 },
+  //base: '/MyPortifolio'
 });
