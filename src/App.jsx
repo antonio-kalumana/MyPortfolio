de12f2cb03d47228a029d2481884bee6cdc41a84
António Kalumana <ak@kalumana-mbp.lan>
2025-06-16 16:29:26 +0100

    fix: react-router support for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home.jsx';
 import './App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
